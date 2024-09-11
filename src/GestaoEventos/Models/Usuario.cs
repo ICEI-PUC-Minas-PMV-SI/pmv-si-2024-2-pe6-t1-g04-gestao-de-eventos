5d410bceb43e6d5d2c34d7110e3b076d04f66aab
ginadev-dutra <gina_20_01@hotmail.com>
2024-09-11 19:37:54 -0300

    Criação e Configuração do modelo de Usuário

--- src/GestaoEventos/Models/Usuario.cs
+++ src/GestaoEventos/Models/Usuario.cs
@@ -1,3 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GestaoEventos.Models;
 
+[Table("Usuarios")]
+public class Usuario
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; }
+    
+    [Required]
+    public int CodigoPessoa { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Telefone { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public Perfil Perfil { get; set; }
+
+}
+
+public enum Perfil
+{
+    [Display(Name = "Administrador")]
+    Aluno,
+    [Display(Name = "Professor")]
+    Professor,
+    [Display(Name = "Funcionario")]
+    Funcionario
+}
 
